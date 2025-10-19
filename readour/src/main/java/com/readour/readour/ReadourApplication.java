f2eaa7225ccab52205d04e11d5e5185687120331
y2kminusone <4pril17@naver.com>
2025-10-20 04:39:00 +0900

    first commit

---
+++ readour/src/main/java/com/readour/readour/ReadourApplication.java
@@ -0,0 +1,13 @@
+package com.readour.readour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadourApplication.class, args);
+	}
+
+}
---
+++ readour/src/test/java/com/readour/readour/ReadourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readour.readour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
