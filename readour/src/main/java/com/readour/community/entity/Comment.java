c3a7a3e8feb80b39b531a8b255f8f9073e2ca7eb
clydejeomo <20230097@kumoh.ac.kr>
2025-11-04 21:30:30 +0900

    [fix] comment 유저명 하드코딩 해제

--- readour/src/main/java/com/readour/community/entity/Comment.java
+++ readour/src/main/java/com/readour/community/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.readour.community.entity;
 
+import com.readour.common.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.*;
@@ -12,8 +13,9 @@ public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
     private Long postId;
-    private Long userId;
-    private Long parentCommentId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
     @Lob private String content;
     private Boolean isDeleted = false;
     private Boolean isHidden = false;
